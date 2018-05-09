a106c168c8e1ee6aebe8a50a157926fb4e42317e
Stephen Wratten <swratten1994@gmail.com>
2018-05-08 23:12:56 -0700

    Fixed Spacing of mad libs code.

--- MiniProject/KirstenBrandon/test.py
+++ MiniProject/KirstenBrandon/test.py
@@ -18,12 +18,12 @@ from PyDictionary import PyDictionary
 dictionary = PyDictionary()
 go = ""
 class MADLIBS():
-    running = True
-    catch = ""
-    finishHit = ""
-    go = ""
+    
     def __init__(self):
-        pass
+        running = True
+        catch = ""
+        finishHit = ""
+        go = ""
 
     def start(self):
         print
@@ -41,7 +41,7 @@ class MADLIBS():
         return self.player()     
 
     def player(self):
-        name = raw_input("Player, what is your name? ")
+        name = input("Player, what is your name? ")
         print 
         print ("Hello %s, lets get started...\n" % name)
         return self.game_select(name)
@@ -52,7 +52,7 @@ class MADLIBS():
         for game in games:
             print (game)
             print
-        selected = raw_input("Which game would you like to play? ").lower()
+        selected = input("Which game would you like to play? ").lower()
         if selected in games:
             if selected == "baseball":
                 go = "baseball"
@@ -71,7 +71,7 @@ class MADLIBS():
         print ("\n")                                                                # Create a space 
         print ("Please provide me with each of the follow...\n")                    # Instructs the user to provide information
 
-        adj = raw_input("Please enter an adjective. ")                              # Instructs the user to enter an adjective
+        adj = input("Please enter an adjective. ")                              # Instructs the user to enter an adjective
         adj = adjective(adj)
 
         #from PyDictionary import PyDictionary                                       # Imports a dictionary form the python libraries   
@@ -84,11 +84,11 @@ class MADLIBS():
             #else:
                 #print ("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \nThe word you enter is not the correct part of speech. Here is the meaning of the word you entered. \n")
                 #print (meaning, "\n")
-                #adj = raw_input("Please try another word. ")
+                #adj = input("Please try another word. ")
                 #meaning = dictionary.meaning(adj)
                 #print ("\n")                
 
-        adj1 = raw_input("please enter another adjective. ")
+        adj1 = input("please enter another adjective. ")
         adj1 = adjective(adj1)
         #meaning = dictionary.meaning(adj1)
         #stop = False
@@ -98,11 +98,11 @@ class MADLIBS():
             #else:
                 #print ("\n \n The word you enter is not the correct part of speech. Here is the meaning of the word you entered. \n")
                 #print (meaning, "\n")
-                #adj1 = raw_input("Please try another word. ")
+                #adj1 = input("Please try another word. ")
                 #meaning = dictionary.meaning(adj1)
                 #print ("\n")
 
-        noun = raw_input("Please enter a noun ")
+        noun = input("Please enter a noun ")
         noun = checkNoun(noun)
         #meaning = dictionary.meaning(noun)
         #print (noun," ", meaning)
@@ -113,7 +113,7 @@ class MADLIBS():
             #else:
                 #print ("\n \n The word you enter is not the correct part of speech. Here is the meaning of the word you entered. \n")
                 #print (meaning, "\n")
-                #noun = raw_input("Please try another word. ")
+                #noun = input("Please try another word. ")
                 #meaning = dictionary.meaning(noun)
                 #print ("\n")
 
@@ -125,20 +125,20 @@ class MADLIBS():
         print ("\n")
         print ("Please provide me with each of the follow...\n")
 
-        nouns = raw_input("Enter the Brewers score. This number is a noun. ")
+        nouns = input("Enter the Brewers score. This number is a noun. ")
         print ("\n")
         brewersScore = nouns
         score1 = int(nouns)
 
-        nouns = raw_input("Enter the Yankees score. This number is a noun. ")
+        nouns = input("Enter the Yankees score. This number is a noun. ")
         print ("\n")
         yankeesScore = nouns
         score2 = int(nouns)
 
-        adj1 = raw_input("Enter a inning number 1-9. This number is an adjective to the Inning. Inning is a noun. ")
+        adj1 = input("Enter a inning number 1-9. This number is an adjective to the Inning. Inning is a noun. ")
         print ("\n")
 
-        nounRunners = raw_input("How may runners on bases? This number is noun. ")
+        nounRunners = input("How may runners on bases? This number is noun. ")
         print ("\n")
        
         while not nounRunners in ("No runners on base.", "Runner on first base.", "Runners on first and second base.", "Bases loaded."):
@@ -152,10 +152,10 @@ class MADLIBS():
                 nounRunners = "Bases loaded."
             else:
                 print ("Please enter a number between 0 to 3.")
-                nounRunners = raw_input("How may runners on bases? This number is noun.")
+                nounRunners = input("How may runners on bases? This number is noun.")
         
 
-        out = raw_input("Enter the number of out(s). ")
+        out = input("Enter the number of out(s). ")
         print ("\n")
 
         while not out in ("no outs.", "one out.", "two outs."):
@@ -167,25 +167,25 @@ class MADLIBS():
                 out = "two outs."
             else:
                 print ("Please enter a number between 0 to 2.")
-                out = raw_input("Enter the number of out(s).")
+                out = input("Enter the number of out(s).")
 
-        ball = raw_input("Enter the number of ball(s). ")
+        ball = input("Enter the number of ball(s). ")
         print ("\n")
 
         while not ball in ("0", "1", "2", "3"):
             if ball < "0" or ball > "3":
                 print ("Please enter a number between 0 to 3.")
-                out = raw_input("Enter the number of out(s).")
+                out = input("Enter the number of out(s).")
 
-        strike = raw_input("Enter the number of strike(s). ")
+        strike = input("Enter the number of strike(s). ")
         print ("\n")
 
         while not strike in ("0", "1", "2"):
             if strike < "0" or strike > "2":
                 print ("Please enter a number between 0 to 2.")
-                out = raw_input("Enter the number of strike(s).")
+                out = input("Enter the number of strike(s).")
 
-        play = raw_input("Enter 'catch' or 'homerun' ")
+        play = input("Enter 'catch' or 'homerun' ")
         print ("\n")
         
         while not play in ("catch", "homerun"):
@@ -200,7 +200,7 @@ class MADLIBS():
                 brewersScore = "5"                  
             else:
                 print ("Please enter 'catch' or 'homerun' only")
-                play = raw_input("Enter 'catch' or 'homerun' ")           
+                play = input("Enter 'catch' or 'homerun' ")           
               
         if play == "catch":
             catch =  "makes the catch. Oh! my god, he made an exceptional catch against the wall robbing"
@@ -213,6 +213,7 @@ class MADLIBS():
             runDiff = score1 - score2
             scenerio = "Yankees down by"
         else:
+            runDiff = 0
             scenerio = "Score is tied"
         
         period = "."
@@ -224,29 +225,29 @@ class MADLIBS():
             
     def ml_weird_day(self, name):
         print ("Please provide me with each of the follow...\n")
-        noun1 = raw_input("A noun: ")
+        noun1 = input("A noun: ")
         noun1 = checkNoun(noun1)
 
-        adj1 = raw_input("An adjective: ")
+        adj1 = input("An adjective: ")
         adj1 = adjective(adj1)
 
-        noun2 = raw_input("A plural noun: ")
+        noun2 = input("A plural noun: ")
         noun2 = checkNoun(noun2)
 
-        famous = raw_input("The name of a famous person: ")
-        place = raw_input("A place: ")
-        verbing1 = raw_input("A verb ending in 'ing': ")
+        famous = input("The name of a famous person: ")
+        place = input("A place: ")
+        verbing1 = input("A verb ending in 'ing': ")
 
-        adj2 = raw_input("Another adjective: ")
+        adj2 = input("Another adjective: ")
         adj2 = adjective(adj2)
 
-        song = raw_input("Your favorite song: ")
-        verbed = raw_input("A verb ending in 'ed': ")
-        adverb = raw_input("An adverb: ")
-        verbing2 = raw_input("Another verb ending in 'ing': ")
-        model = raw_input("The name of a supermodel: ")
+        song = input("Your favorite song: ")
+        verbed = input("A verb ending in 'ed': ")
+        adverb = input("An adverb: ")
+        verbing2 = input("Another verb ending in 'ing': ")
+        model = input("The name of a supermodel: ")
 
-        adj3 = raw_input("One final adjective: ")
+        adj3 = input("One final adjective: ")
         adj3 = adjective(adj3)
 
         STORY = "Once upon a time there was a %s. It had %s %s! One day it met %s on the side of the %s they were %s. It was very %s; they both looked like hobos! All of the sudden they started singing %s really loudly. They %s really %s! %s started %s with %s. They looked really %s!"
@@ -264,7 +265,7 @@ class MADLIBS():
             else:
                 print ("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \nThe word you enter is not the correct part of speech. Here is the meaning of the word you entered. \n")
                 print (meaning, "\n")
-                adj = raw_input("Please try another word. ")
+                adj = input("Please try another word. ")
                 word = dictionary.meaning(adj)
                 print ("\n")
         return word
@@ -280,7 +281,7 @@ class MADLIBS():
             else:
                 print ("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \nThe word you enter is not the correct part of speech. Here is the meaning of the word you entered. \n")
                 print (meaning, "\n")
-                adj = raw_input("Please try another word. ")
+                adj = input("Please try another word. ")
                 word = dictionary.meaning(adj)
                 print ("\n")
         return word
